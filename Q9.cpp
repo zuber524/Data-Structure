15c8d97c9abd7fcd5cb0d640c99083cbb1c76454
Zuber KHAN <51720338+zuber524@users.noreply.github.com>
2019-11-05 22:39:50 +0530

    Add files via upload

--- Q9.cpp
+++ Q9.cpp
@@ -1,101 +1,81 @@
 //18/94087
-//q13.polynomial using linked list
+//postfix using stack;
 
-#include<iostream>
-using namespace std;
+#include "STACK.cpp"
 
-template <class T>
-struct node
+class pf
 {
-	T coef,pow;	
-	node *link;
-
 	public:
-	node(T coef=0, T pow=0,node *link=0)
+	double postfix(string s)
 	{
-		this->coef=coef;
-		this->pow=pow;
-		this->link=link;
-	}
-};
-
-template <class T>
-class poly
-{
-	node <T>*head;
-	public:
-	void input()
-	{
-		head=0;
-		node <T>*temp,*nn;
-
-		cout<<"\nHighest power of polynomial=";
-		T n,num;	cin>>n;	
-		cout<<"\nCoefficient=\n";	
-		while(n!=-1)
+		int i=0,n=s.length();
+		stack <double>st(n);
+		while(s[i]!='\0')
 		{
-			cout<<"x^"<<n<<"=";	cin>>num;
-			nn=new node<T> (num,n);
-			if(!head)	head=temp=nn;
-			else	temp=temp->link=nn;
-			n--;
-		}
-		temp=0;
-		delete temp;
-	}
-	
-	void show()
-	{
-		node<T> *t=head;	char c='+';
-		while(t)
-		{
-			c=t->coef>0?'+':'\0';
-			if(t->coef==0){}
-			else 
-			{	
-				cout<<c<<t->coef;
-				if(t->pow!=0)	cout<<"x^"<<t->pow;
+			if(isdigit(s[i]))
+			st.push(s[i++]-'0');	
+			
+			else if(isoperator(s[i]))
+			{
+				int a,b; double s1;
+				if(!st.isempty())
+				{
+					b=st.pop();
+					if(!st.isempty())	a=st.pop();
+					else
+					{
+						cout<<"Invalid expression1.";
+						return -999;
+					}
+				}
+				else
+				{
+					cout<<"Invalid expression1.";
+					return -999;
+				}
+				switch(s[i])
+				{
+					case '+':s1=a+b;	break;
+					case '-':s1=a-b;	break;
+					case '*':s1=a*b;	break;
+					case '/':s1=a*1.0/b;	break;
+				}
+				st.push(s1);
+				i++;
 			}
-			t=t->link;
-		}
-		t=0;	delete t;
-	}
-	
-	void add()
-	{
-		head=0;
-		cout<<"\n1st polynomial...";
-		poly<T> a;	a.input();
-		cout<<"\n2nd polynomial...";
-		poly<T> b;	b.input();
-		node<T> *t1=a.head,*t2=b.head, *nn,*temp;
-		while(t1)
-		{
-			if(t1->pow>t2->pow)
-			{	nn=new node<T>(t1->coef,t1->pow);	t1=t1->link;	}
-			else if(t1->pow<t2->pow)
-			{	nn=new node<T>(t2->coef,t2->pow);		t2=t2->link;	}
 			else
-			{	
-				nn=new node<T>(t1->coef+t2->coef,t1->pow);	
-				t1=t1->link;	t2=t2->link;
+			{
+				cout<<"Invalid expression2.";
+				return -999;
 			}
-			if(!head)	head=temp=nn;
-			else	temp=temp->link=nn;
 		}
-		cout<<"\n1st polynomial=";	a.show();
-		cout<<"\n2nd polynomial=";	b.show();
-		cout<<"\nSum of both polynomial=";	show();
+		if(st.top==0)	 return st.pop();	
+		else	cout<<"Invalid expression3.";
 	}
 	
+	bool isdigit(char x)
+	{
+		int c=(int)x;
+		if(c>47&&c<=57)	return true;
+		else 	return false;
+	}
 	
+	bool isoperator(char x)
+	{	
+		switch(x)
+		{
+			case '+':case '-':case '*':case '/':return true;
+			default:return false;
+		}
+	}
 };
-
 int main()
 {
-	cout<<"\n\n*******POLYNOMIAL USING LINKED LIST*******\n\n";
-	poly <float>x;		//reference variable of class poly
-	x.add();	cout<<"\n";
+	cout<<"Expression=";
+	string s;cin>>s;
+	pf a;
+	cout<<"\nFinal value="<<a.postfix(s)<<"\n";
 	return 0;
 }
-
+	
+				
